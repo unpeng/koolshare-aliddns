5cfc77449daff3b3989b5ddbcd2558774d285bdc
wangp1982 <wangp1982@hotmail.com>
2017-08-28 16:25:40 +0800

    额

--- aliddns/install.sh
+++ aliddns/install.sh
@@ -5,5 +5,5 @@ chmod a+x /koolshare/scripts/aliddns_*
 
 # add icon into softerware center
 dbus set softcenter_module_aliddns_install=1
-dbus set softcenter_module_aliddns_version=0.4
+dbus set softcenter_module_aliddns_version=0.1
 dbus set softcenter_module_aliddns_description="USB温控"
