c3a062975941f082f5ea2890bd848d8ae75e310f
wangp1982 <wangp1982@hotmail.com>
2017-08-28 16:23:48 +0800

    萨芬

--- aliddns/install.sh
+++ aliddns/install.sh
@@ -6,4 +6,4 @@ chmod a+x /koolshare/scripts/aliddns_*
 # add icon into softerware center
 dbus set softcenter_module_aliddns_install=1
 dbus set softcenter_module_aliddns_version=0.4
-dbus set softcenter_module_aliddns_description="阿里云解析自动更新IP"
+dbus set softcenter_module_aliddns_description="USB温控"
